9584309793a293c243b30c53c84d4fe24310039d
Zoe Keller <keller.zoe98@gmail.com>
2020-06-18 22:39:28 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,50 @@
+
+
+class Artist 
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end 
+
+  def self.all
+    @@all 
+  end 
+  
+  def Artist.destroy_all 
+    @@all.clear 
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.create(artist)
+    artist = self.new(artist)
+    artist.save 
+    artist 
+  end 
+  
+  def songs 
+    @songs 
+  end 
+  
+  def add_song(song)
+    if song.artist == nil 
+      song.artist = self 
+    else 
+      nil 
+    end 
+    if @songs.include?(song)
+      nil
+    else 
+      @songs << song 
+    end 
+    song 
+  end 
+  
+  
+end 
---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song 
+  attr_accessor :name, :artist, :genre, :musicimporter, :musiclibrarycontroller
+  extend Concerns::Findable 
+  
+  @@all = []
+  def initialize(name, artist=nil, genre=nil)
+    @name = name 
+    self.artist=(artist) if artist != nil
+    self.genre=(genre) if genre != nil 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def name=(song_name)
+    @name=song_name
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.create(song)
+    song = self.new(song)
+    song.save 
+    song 
+  end 
+end 
